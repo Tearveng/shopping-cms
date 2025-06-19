bf3c1eceec5c56ac9ee997c51fd77dd4a3f27b6d
Veng TEA <veng.tea@allweb.com.kh>
2025-06-19 16:42:23 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/admin/",
   plugins: [vue()],
   css: {
     preprocessorOptions: {
       less: {
         modifyVars: {
-          'primary-color': '#1DA57A',
-          'link-color': '#1DA57A',
+          "primary-color": "#1DA57A",
+          "link-color": "#1DA57A",
         },
         javascriptEnabled: true,
       },
     },
   },
-})
+});
