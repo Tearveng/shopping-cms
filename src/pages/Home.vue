578459696ca245f7022885b1cf224465423d7aec
Yong <yong@Yongs-MacBook-Pro.local>
2026-03-17 21:16:13 +0700

    change

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -9,13 +9,5 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-
 const pro_link = import.meta.env.VITE_PRODUCTION_LINK
-
-const subdomain = computed(() => {
-  const hostname = window.location.hostname; // e.g., "sub.example.com"
-  const parts = hostname.split(".");
-  return parts.length > 2 ? parts[0] : ""; // Returns "sub" or empty string
-});
 </script>
